c75e917ac9856f23c09d01295fdd3d8a29d577b7
Manuell00 <manuell.caselli@gmail.com>
2023-05-30 15:27:58 +0200

    first commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<!-- Inserisco il template -->
+<template>
+    <h1 id="title">La mia prima app con Vite!</h1>
+</template>
+
+
+<!-- Inserisco lo stile  -->
+<style scoped>
+    #title{
+        font-size: 50px;
+        font-weight: bold;
+        color: dodgerblue;
+    }
+</style>
